5e365d6fc8afa46b6cb9a5401932b2456d94874c
云荷YunHerry <54676890+YunHerry@users.noreply.github.com>
2022-06-24 23:53:31 +0800

    refactor
    添加了日志引擎

---
+++ src/indi/yunHerry/log/InfoPrintExecute.java
@@ -0,0 +1,11 @@
+package indi.yunHerry.log;
+
+import java.io.PrintStream;
+
+/**
+ * @author YunHerry
+ * 信息打印类
+ */
+public class InfoPrintExecute {
+    protected PrintStream out = System.out;
+}
