ecc42c2889f3ee2a61910d7dc963307eb9d5cce0
云荷YunHerry <54676890+YunHerry@users.noreply.github.com>
2022-06-26 20:56:07 +0800

    feat
    判断命令格式是否正确

--- src/indi/yunHerry/Terminal.java
+++ src/indi/yunHerry/Terminal.java
@@ -1,9 +1,11 @@
 package indi.yunHerry;
 
+import indi.yunHerry.exception.ParameterParsingException;
 import indi.yunHerry.log.InfoPrintExecute;
 
 import java.io.InputStream;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 /**
  * @author YunHerry
@@ -11,14 +13,21 @@ import java.util.Scanner;
  */
 public class Terminal {
     protected static Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws ParameterParsingException {
         Terminal.run();
     }
-    public static void run() {
-       String command;
+
+    public static void run() throws ParameterParsingException {
+        String command;
         do {
-          command = scanner.nextLine();
-          InfoPrintExecute.InfoPrint(command);
+            System.out.print("> ");
+            command = scanner.nextLine();
+            System.out.println(Pattern.matches("^/[a-zA-Z0-9]{1,}+", command));
+            if (!Pattern.matches("^/[a-zA-Z0-9]{1,}+", command)) {
+                throw new ParameterParsingException("无法解析该命令");
+            }
+            InfoPrintExecute.InfoPrint(command);
         } while (!"exit".equalsIgnoreCase(command));
     }
 }
