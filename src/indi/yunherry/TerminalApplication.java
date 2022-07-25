323d032027113ced2854b2f6f3d253b1a8b372a4
云荷YunHerry <54676890+YunHerry@users.noreply.github.com>
2022-07-25 17:42:00 +0800

    feat
    移除了gradle
    完善了配置的读取

--- src/indi/yunherry/TerminalApplication.java
+++ src/indi/yunherry/TerminalApplication.java
@@ -1,6 +1,7 @@
 package indi.yunherry;
 
 
+import indi.yunherry.configloader.ConfigLoader;
 import indi.yunherry.factory.factory.Factory;
 import indi.yunherry.model.dto.TerminalContext;
 
@@ -11,6 +12,7 @@ import indi.yunherry.model.dto.TerminalContext;
 public class TerminalApplication {
     public static TerminalContext run(Class<?> clazz) throws Exception {
         Factory.initFactory(clazz);
+        ConfigLoader.load(clazz);
         return TerminalContext.run();
     }
 }
